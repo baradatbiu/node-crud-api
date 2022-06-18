51b6aece5ab8e055e9753058c8d0685f1c86744a
Dmitry Kharlamov <newdimaak@gmail.com>
2022-06-18 04:09:38 +0300

    feat: add types

---
+++ src/types/User.ts
@@ -0,0 +1,37 @@
+export interface User {
+  id?: string;
+  username: string;
+  age: number;
+  hobbies: string[];
+}
+
+export type Users = User[];
+
+export const USERS_API_ENDPOINT = "/api/users";
+
+export enum ROUTE {
+  GET_USER_BY_ID = "GET_USER_BY_ID",
+  GET_USERS = "GET_USERS",
+  POST_USER = "POST_USER",
+  PUT_USER = "PUT_USER",
+  DELETE_USER = "DELETE_USER",
+}
+
+export enum ERRORS {
+  INVALID_METHOD = "Invalid method",
+  INVALID_ROUTE = "Invalid route",
+  INVALID_PARAMS = "Invalid params",
+  INVALID_ENDPOINT = "Invalid api endpoint",
+  USER_NOT_FOUND = "User not found",
+  INVALID_USER = "User not valid",
+  INTERNAL_SERVER = "Internal server error",
+}
+
+export enum HTTP_STATUS_CODE {
+  OK = 200,
+  OK_ADD = 201,
+  OK_DELETE = 204,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER = 500,
+}
